8c24a98981fd8c138fcd3d4df64753d710aa7783
LindaPermata <permatalinda531@gmail.com>
2024-05-13 17:14:49 +0700

    membuat void angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -13,8 +13,22 @@ public:
      void isiData();
 };
 //definisi member function
-angka::angka(int i){
+angka::angka(int i){ //constructor
     panjang = i;
     arr = new int[i];
     isiData();
+}
+
+angka::~angka(){ //destructor
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+
+}
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
 }
